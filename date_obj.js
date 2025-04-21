828ea709766664fbbbb40cb1edec8a9b9cb5fb09
Divyansh-Chaubey <chaubeydivyansh91@gmail.com>
2025-04-21 17:40:21 +0530

    update

---
+++ date_obj.js
@@ -0,0 +1,15 @@
+let date=new Date()
+console.log(date.getFullYear());
+console.log(date);
+console.log(date.toLocaleString());
+console.log(date.getMonth());
+console.log(date.getMonth()+1);
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+
+
+let arr=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
+console.log(arr[date.getDay()]);
---
+++ math.js
@@ -0,0 +1,29 @@
+// Demonstrating the use of the Math object in JavaScript
+
+// Math constants
+console.log("PI:", Math.PI);
+console.log("E:", Math.E);
+
+// Rounding methods
+console.log("Round 4.7:", Math.round(4.7));
+console.log("Ceil 4.1:", Math.ceil(4.1));
+console.log("Floor 4.9:", Math.floor(4.9));
+
+// Power and square root
+console.log("2^3:", Math.pow(2, 3));
+console.log("Square root of 16:", Math.sqrt(16));
+
+// Trigonometric functions
+console.log("Sin(90 degrees):", Math.sin((90 * Math.PI) / 180));
+console.log("Cos(0 degrees):", Math.cos(0));
+
+// Random number
+console.log("Random number (0 to 1):", Math.random());
+console.log("Random number (1 to 10):", Math.floor(Math.random() * 10) + 1);
+
+// Absolute value
+console.log("Absolute of -5:", Math.abs(-5));
+
+// Max and Min
+console.log("Max of [3, 5, 7]:", Math.max(3, 5, 7));
+console.log("Min of [3, 5, 7]:", Math.min(3, 5, 7));
